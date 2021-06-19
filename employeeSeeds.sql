c9e60861db945b91bd548623947bbd401bd23d59
AnaMolly <analia.mohamdally@gmail.com>
2021-06-19 19:05:13 -0400

    initialized npm and installed packages. also added code to connect to the database

---
+++ employeeSeeds.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employeeDB;
+
+CREATE DATABASE  employeeDB;
+
+USE  employeeDB;
+
+CREATE TABLE department (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  roles_id INT NOT NULL,
+  manager_id INT
+);
